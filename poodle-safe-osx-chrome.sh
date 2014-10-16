02547dbf1dc158ae234dbf57630ec66ec3775cbc
Rebecca Turner <turner@mikomi.org>
2014-10-16 18:50:25 -0400

    Make it able to repair its earlier bug

--- poodle-safe-osx-chrome.sh
+++ poodle-safe-osx-chrome.sh
@@ -4,17 +4,26 @@ CHROME="${1-/Applications/Google Chrome.app}"
 EXEC="$CHROME/Contents/MacOS/Google Chrome"
 POODLED="$CHROME/Contents/MacOS/Google Chrome Poodled"
 
+function poodle() {
+    cat << EOF > "$EXEC"
+#!/bin/sh
+exec "\$0 Poodled" --ssl-version-min=tls1 "\$@"
+EOF
+}
+
 if [ -f "$POODLED" ]; then
+	if grep -q poodle-safe-osx-chrome.sh "$EXEC"; then
+		echo "I made your chrome bad, sorry about that! =( Fixing it!"
+		poodle
+		exit 0
+	fi
 	echo "You already made your chrome poodle-safe" 1>&2
 	exit 1
 fi
 
 mv "$EXEC" "$POODLED"
 
-cat << EOF > "$EXEC"
-#!/bin/sh
-exec "\$0 Poodled" --ssl-version-min=tls1 "\$@"
-EOF
+poodle
 
 chmod a+rx "$EXEC"
 

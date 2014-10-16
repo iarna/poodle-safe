7b1df58b209d09004f6be9abf98c9b3381b4d62b
Rebecca Turner <turner@mikomi.org>
2014-10-16 18:22:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Make your OSX Chrome Poodle Safe in one easy step:
+
+```sh
+curl https://raw.githubusercontent.com/iarna/poodle-safe/master/poodle-safe-osx-chrome.sh | bash
+```
---
+++ poodle-safe-osx-chrome.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e -o pipefail
+CHROME="${1-/Applications/Google Chrome.app}"
+EXEC="$CHROME/Contents/MacOS/Google Chrome"
+POODLED="$CHROME/Contents/MacOS/Google Chrome Poodled"
+
+if [ -f "$POODLED" ]; then
+	echo "You already made your chrome poodle-safe" 1>&2
+	exit 1
+fi
+
+mv "$EXEC" "$POODLED"
+
+cat << EOF > "$EXEC"
+#!/bin/sh
+exec "$0 Poodled" --ssl-version-min=tls1 "$@"
+EOF
+
+chmod a+rx "$EXEC"
+
+echo "Your Chrome is now poodle safe. Please quit and relaunch it."
+echo "You'll need to approve Google Chrome Poodled for keychain access."
